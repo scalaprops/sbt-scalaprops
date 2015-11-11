ce50c724cb59f8900e0f1d338f7144b44179100e
xuwei-k <6b656e6a69@gmail.com>
2015-11-12 01:48:02 +0900

    0.1.16

--- project/Build.scala
+++ project/Build.scala
@@ -21,7 +21,7 @@ object build extends Build {
     ),
     ScriptedPlugin.scriptedLaunchOpts ++= Seq(
       "-Dplugin.version=" + version.value,
-      "-Dscalaprops.version=0.1.15"
+      "-Dscalaprops.version=0.1.16"
     ),
     startYear := Some(2015),
     organization := "com.github.scalaprops",
