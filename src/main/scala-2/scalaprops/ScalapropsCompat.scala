181e124623704a27907a35a8c2cf0b439695bbe1
kenji yoshida <6b656e6a69@gmail.com>
2026-04-07 13:06:44 +0900

    sbt 2 scala-native

--- src/main/scala-2/scalaprops/ScalapropsCompat.scala
+++ src/main/scala-2/scalaprops/ScalapropsCompat.scala
@@ -1,7 +1,18 @@
 package scalaprops
 
+import java.io.File
+import xsbti.FileConverter
+
 private[scalaprops] object ScalapropsCompat {
   implicit class DefOps(private val self: sbt.Def.type) extends AnyVal {
     def uncached[A](a: A): A = a
   }
+
+  def fileToVirtualFileRef(f: File, converter: FileConverter): File =
+    f
+
+  def virtualFileRefToFile(f: File, converter: FileConverter): File =
+    f
+
+  def testResult: Unit = ()
 }
--- src/main/scala-3/scalaprops/ScalapropsCompat.scala
+++ src/main/scala-3/scalaprops/ScalapropsCompat.scala
@@ -1,3 +1,16 @@
 package scalaprops
 
-private[scalaprops] object ScalapropsCompat
+import java.io.File
+import xsbti.FileConverter
+import xsbti.VirtualFileRef
+import sbt.protocol.testing.TestResult
+
+private[scalaprops] object ScalapropsCompat {
+  inline def fileToVirtualFileRef(f: File, converter: FileConverter): VirtualFileRef =
+    converter.toVirtualFile(f.toPath)
+
+  inline def virtualFileRefToFile(ref: VirtualFileRef, converter: FileConverter): File =
+    converter.toPath(ref).toFile
+
+  inline def testResult: TestResult = TestResult.Passed
+}
