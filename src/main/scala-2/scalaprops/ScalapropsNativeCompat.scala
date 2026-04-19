3db2ffbc9132f116c0053ae71da7e5d6c2a0c042
xuwei-k <6b656e6a69@gmail.com>
2026-04-20 07:17:34 +0900

    Overwrite test

--- src/main/scala-2/scalaprops/ScalapropsNativeCompat.scala
+++ src/main/scala-2/scalaprops/ScalapropsNativeCompat.scala
@@ -2,4 +2,6 @@ package scalaprops
 
 private[scalaprops] trait ScalapropsNativeCompat { self: ScalapropsNativePlugin.type =>
   final val testFullKey = sbt.Keys.test
+  final val testTaskSetting: Seq[sbt.Def.Setting[?]] =
+    Nil
 }
