9e32778ed4eba7bc0fb3029bb59382efa80ba160
kenji yoshida <6b656e6a69@gmail.com>
2026-06-27 07:38:19 +0900

    Refactor UpdateReadme.scala for prepare sbt 2

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -6,15 +6,14 @@ object UpdateReadme {
 
   val updateReadmeTask = { (state: State) =>
     val extracted = Project.extract(state)
-    val v = extracted get version
-    val org = extracted get organization
-    val n = extracted get name
-    val snapshotOrRelease = if (extracted get isSnapshot) "snapshots" else "releases"
+    val v = extracted.get(version)
+    val org = extracted.get(organization)
+    val n = extracted.get(name)
+    val snapshotOrRelease = if (extracted.get(isSnapshot)) "snapshots" else "releases"
     val readme = "README.md"
     val readmeFile = file(readme)
-    val newReadme = Predef
-      .augmentString(IO.read(readmeFile))
-      .lines
+    val newReadme = IO
+      .readLines(readmeFile)
       .map { line =>
         val matchReleaseOrSnapshot = line.contains("SNAPSHOT") == v.contains("SNAPSHOT")
         if (line.startsWith("addSbtPlugin") && matchReleaseOrSnapshot) {
@@ -23,7 +22,7 @@ object UpdateReadme {
       }
       .mkString("", "\n", "\n")
     IO.write(readmeFile, newReadme)
-    val git = new Git(extracted get baseDirectory)
+    val git = new Git(extracted.get(baseDirectory))
     git.add(readme) ! state.log
     git.commit(message = "update " + readme, sign = false, signOff = false) ! state.log
     sys.process.Process("git diff HEAD^") ! state.log
